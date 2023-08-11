b49cfda9f5bd7de6997be3434d42da74055d69d6
io <simoneroldo04@gmail.com>
2023-08-11 10:07:18 +0200

    Initial Commit

---
+++ Habit/HabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitApp.swift
+//  Habit
+//
+//  Created by simone roldo on 11/08/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
